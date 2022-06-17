fef3acb63454f5be3b51fb3cea284a4a85653ddb
hecsalazarf <36686213+hecsalazarf@users.noreply.github.com>
2022-06-17 14:26:25 -0500

    test: Add integration test for event queue registration

---
+++ tests/events.rs
@@ -0,0 +1,30 @@
+use dotenv::dotenv;
+use rulip::{Client, Error};
+use std::env;
+use std::sync::Once;
+static INIT: Once = Once::new();
+
+pub fn initialize() {
+    INIT.call_once(|| {
+        dotenv().ok();
+    });
+}
+
+#[tokio::test]
+#[ignore]
+async fn register() -> Result<(), Error> {
+    initialize();
+    let username = env::var("ZULIP_USERNAME").expect("Zulip username");
+    let api_key = env::var("ZULIP_API_KEY").expect("Zulip API key");
+    let uri = env::var("ZULIP_URI").expect("Zulip URI");
+
+    let client = Client::build(uri)
+        .with_key(username, api_key)
+        .init()
+        .await?;
+    let queue = client.queue().register().await?;
+    println!("Queue registered with ID: '{}'", queue.id());
+    assert!(queue.id().len() > 0);
+    assert_eq!(queue.last_event_id(), -1);
+    Ok(())
+}
